a15e4d6c6126581d348427c49bf9f307b34ba17f
Joey Register <josephregister1258@gmail.com>
2020-04-19 16:05:23 -0400

    Updates

---
+++ precache-manifest.abcb04419f892c10d5092940d7f1ed77.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdfff18433663659a205f677b069ca0",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "9eebdec57ed5d1494198",
+    "url": "/ReactPortfolio/static/css/main.d3d0df9e.chunk.css"
+  },
+  {
+    "revision": "e1882d310bad78ef56d0",
+    "url": "/ReactPortfolio/static/js/2.050c0493.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.050c0493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eebdec57ed5d1494198",
+    "url": "/ReactPortfolio/static/js/main.71c27ee8.chunk.js"
+  },
+  {
+    "revision": "6ccc22b693acd3d55f7c",
+    "url": "/ReactPortfolio/static/js/runtime-main.d41f34d2.js"
+  },
+  {
+    "revision": "06c8e05aeb458266ade1085896c0332f",
+    "url": "/ReactPortfolio/static/media/icomoon.06c8e05a.svg"
+  },
+  {
+    "revision": "8275504f5b9e186b54b8d84ebdedc2b4",
+    "url": "/ReactPortfolio/static/media/icomoon.8275504f.eot"
+  },
+  {
+    "revision": "dd6d51dddf740923fe9c94859aeee2bf",
+    "url": "/ReactPortfolio/static/media/icomoon.dd6d51dd.woff"
+  },
+  {
+    "revision": "ee8d016f1bfe9220bee450ff02b59624",
+    "url": "/ReactPortfolio/static/media/icomoon.ee8d016f.ttf"
+  },
+  {
+    "revision": "a0ab9594a48944a1afea75cfe30b8515",
+    "url": "/ReactPortfolio/static/media/img-1.a0ab9594.jpg"
+  },
+  {
+    "revision": "b62690192e8a978f0130778c2115cd91",
+    "url": "/ReactPortfolio/static/media/img-2.b6269019.jpg"
+  },
+  {
+    "revision": "121ded7a942839460b2a51712c78a384",
+    "url": "/ReactPortfolio/static/media/img-3.121ded7a.jpg"
+  },
+  {
+    "revision": "eb2d62b348d60483cb69650da06c2608",
+    "url": "/ReactPortfolio/static/media/img-4.eb2d62b3.jpg"
+  },
+  {
+    "revision": "34a078985ec0749bbbf63f2147ea85c2",
+    "url": "/ReactPortfolio/static/media/img-5.34a07898.jpg"
+  },
+  {
+    "revision": "2a9751ee0013b8e3d22effc103c9c23e",
+    "url": "/ReactPortfolio/static/media/img-6.2a9751ee.jpg"
+  },
+  {
+    "revision": "e94dc015c0d10e488a2c840c5e3941c8",
+    "url": "/ReactPortfolio/static/media/img_bg_1.e94dc015.jpg"
+  },
+  {
+    "revision": "fc546e84f384d0bbe3469e6bc147887e",
+    "url": "/ReactPortfolio/static/media/img_bg_2.fc546e84.jpg"
+  }
+]);
